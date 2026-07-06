4ebbb8ef467c2acacb6cb5702cabf88d75b114f3
S.R.P.D.D. RAJAWARDHANE <rajawar-cs21062@stu.kln.ac.lk>
2026-07-06 21:46:48 +0530

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/AlchemiX---Data-Analysis-Frontend/",
   server: {
     port: 5173,
     strictPort: true,
